eb1629d9e9f1d85bf4c34859d60590a08d173854
GitHub <action@github.com>
2022-07-01 11:12:25 +0000

    fix: option user

--- src/Feature.php
+++ src/Feature.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Fabricity\Package2;
 
-final class Package2
+final class Feature
 {
     public static function demo(): string
     {
-        return 'demo package2 coool nice';
+        return 'feature';
     }
 }
